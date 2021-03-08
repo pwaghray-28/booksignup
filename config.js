186f8e82fab65e5061ad711f85bdc1c3ebf7aef8
pwaghray-28 <68452593+pwaghray-28@users.noreply.github.com>
2021-03-07 19:25:22 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAtKM1H6E__pT16_tvDsvSCDu5BGQ27mFE",
+  authDomain: "bookdonations-698b2.firebaseapp.com",
+  projectId: "bookdonations-698b2",
+  storageBucket: "bookdonations-698b2.appspot.com",
+  messagingSenderId: "294192773720",
+  appId: "1:294192773720:web:9597c9484805cf17faa82d"
+};
+
+
+
+// Initialize Firebase
+if(!firebase.apps.length)
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
